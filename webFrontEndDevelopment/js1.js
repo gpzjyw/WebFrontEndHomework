552dffdc1a27e76c25c3a62ec6e837f1cdd698ae
JiPeng <gpzjyw@163.com>
2015-11-27 23:55:56 +0800

    实现了ajax获取数据，并利用获得的数据操作DOM

--- webFrontEndDevelopment/js1.js
+++ webFrontEndDevelopment/js1.js
@@ -1,4 +1,7 @@
-//给http尾部添加查询参数
+getHotList();
+getClassList(1,20,10);
+
+//给url尾部添加查询参数
 function addURLComponent(url,name,value){
 	url += (url.indexOf("?") == -1 ? "?" : "&");
 	url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
@@ -11,7 +14,9 @@ function getClassList(pageNo,psize,type){
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4){
 			if((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304){
-				console.log(xhr.responseText);
+				var text = JSON.parse(xhr.responseText);
+				globalTemp2 =text;
+				createProductList(text.list);
 			}else{
 				console.log("Request was unsuccessful：" + xhr.status);
 			}
@@ -25,13 +30,15 @@ function getClassList(pageNo,psize,type){
 	xhr.send(null);
 }
 
-//获得最热排行的JSON数据
+//获取最热排行的JSON数据
 function getHotList(){
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4){
 			if((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304){
-				console.log(xhr.responseText);
+				var text = JSON.parse(xhr.responseText);
+				globalTemp = text;
+				createHotList(text);
 			}else{
 				console.log("Request was unsuccessful：" + xhr.status);
 			}
@@ -47,7 +54,8 @@ function getAttention(){
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4){
 			if((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304){
-				console.log(xhr.responseText);
+				JSON.parse(xhr.responseText);
+
 			}else{
 				console.log("Request was unsuccessful：" + xhr.status);
 			}
@@ -63,7 +71,7 @@ function checkUser(userName,password){
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4){
 			if((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304){
-				console.log(xhr.responseText);
+				JSON.parse(xhr.responseText);
 			}else{
 				console.log("Request was unsuccessful：" + xhr.status);
 			}
@@ -75,4 +83,77 @@ function checkUser(userName,password){
 	xhr.open("get",referUrl,false);
 	xhr.send(null);
 //	xhr.send(userName,password);
+}
+
+function createHotList(text){
+	var hotList = document.getElementById("j-hotlist");
+	for(var i=0; i<10; i++){
+		var data = text[i];
+		var li = document.createElement("li");
+		var img1 = document.createElement("img");
+		var div = document.createElement("div");
+		var a = document.createElement("a");
+		var img2 = document.createElement("img");
+		var span = document.createElement("span");
+
+		li.className = "f-clear";
+		if(i<9) {li.style.cssText = "margin-bottom:20px;"}
+		img1.src = data.smallPhotoUrl;
+		img1.style.cssText = "width:50px; height:50px; float:left;";
+		div.style.cssText = "display:inline-block; width:125px;";
+		a.innerHTML = data.name;
+		a.href = data.providerLink;
+		a.title = data.name;
+		a.style.cssText = "display:inline-block; margin-left:10px; width:123px; overflow:hidden; white-space:nowrap; text-overflow:ellipsis; font:14px '微软雅黑'; text-decoration:none;";
+		img2.src = "pic/personhead.png";
+		img2.style.cssText = "margin-left:10px";
+		span.innerHTML = "  "+data.learnerCount;
+		span.style.cssText = "color:#bbbbbb;";
+
+		li.appendChild(img1);
+		li.appendChild(div);
+		div.appendChild(a);
+		div.appendChild(img2);
+		div.appendChild(span);
+		hotList.appendChild(li);
+	}
+}
+
+function createProductList(text){
+	var productList = document.getElementById("j-classlist");
+
+	for(var i=0;i<20;i++){
+		var data = text[i];
+		var div1 = document.createElement("div");
+		var img1 = document.createElement("img");
+		var a = document.createElement("a");
+		var p = document.createElement("p");
+		var div2 = document.createElement("div");
+		var img2 = document.createElement("img");
+		var span1 = document.createElement("span");
+		var span2 = document.createElement("span");
+
+		div1.style.cssText = "display:inline-block; margin:0px 20px 20px 0px; width:225px; height228px; background-color:#FFFFFF;";
+		img1.src = data.bigPhotoUrl;
+		img1.style.cssText = "width:223px; margin:1px;";
+		a.innerHTML = data.name;
+		a.style.cssText = "";
+		p.innerHTML = data.provider;
+		div2.style.cssText = "";
+		img2.src = "pic/personhead.png";
+		span1.innerHTML = data.learnerCount;
+		span1.style.cssText = "";
+		span2.innerHTML = "¥ "+data.price;
+		span2.style.cssText = "";
+
+		div1.appendChild(img1);
+		div1.appendChild(a);
+		div1.appendChild(p);
+		div1.appendChild(div2);
+		div2.appendChild(img2);
+		div2.appendChild(span1);
+		div1.appendChild(span2);
+		productList.appendChild(div1);
+
+	}
 }
